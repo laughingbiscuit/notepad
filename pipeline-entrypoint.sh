0a8bfb35310bc091a08401145a74cf8ddf20f4f9
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-17 16:01:29 +0000

    var name

--- pipeline-entrypoint.sh
+++ pipeline-entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p target
 cucumber -f pretty -f html -o target/index.html
 
 # remove secrets from report
-cat .github/workflows/pipeline.yml | yq -r '.jobs[].steps[].env[]' | grep -o 'secret.* ' | sed 's/secrets\.//' | while read line; do
+cat .github/workflows/pipeline.yml | yq -r '.jobs[].steps[].env[]' | grep -o 'secret.* ' | sed 's/secrets\.//' | while read SOME_SECRET; do
   sed -i "s/$SOME_SECRET/***/g" target/index.html
 done
 
