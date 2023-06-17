c698e0e56712c1ec8b107affa6b0405bc21aea8b
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-17 16:38:54 +0000

    add a check for secrets

--- pipeline-entrypoint.sh
+++ pipeline-entrypoint.sh
@@ -6,3 +6,6 @@ bundle
 mkdir -p target
 cucumber -f pretty -f html -o target/index.html
 
+cat .github/workflows/pipeline.yml | yq -r '.jobs[].steps[].env[]' | grep -o 'secret.* ' | sed 's/secrets\.//' | while read SOME_SECRET; do
+  ! grep "$(eval \"echo $SOME_SECRET\")" target/index.html
+done
