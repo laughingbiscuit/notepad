7b8b69f7729d77a522be12e9c858ceb1c026bc8b
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-18 14:33:09 +0000

    detect leaked secrets in target report

--- pipeline-entrypoint.sh
+++ pipeline-entrypoint.sh
@@ -7,5 +7,5 @@ mkdir -p target
 cucumber -f pretty -f html -o target/index.html
 
 cat .github/workflows/pipeline.yml | yq -r '.jobs[].steps[].env[]' | grep -o 'secret.* ' | sed 's/secrets\.//' | while read SOME_SECRET; do
-  ! grep "$(eval \"echo $SOME_SECRET\")" target/index.html
+  ! grep eval "echo $"$VAR"" target/index.html
 done
