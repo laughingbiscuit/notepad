d29721b6cff2a871fae7eb58a3f9ca37d8800147
laughingbiscuit <sean@laughingbiscuit.com>
2023-06-25 15:51:04 +0000

    debug before

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -16,7 +16,8 @@ end
 Before do |scenario|
   @cmdsnip = ""
   @host = "http://localhost"
-  stdout, stderr, status = Open3.capture3("docker run -d -p 80:80 kennethreitz/httpbin && sleep 2")
+  stdout, stderr, status = Open3.capture3("docker run -d -p 80:80 kennethreitz/httpbin && sleep 2 2>&1")
+  attach(stdout, 'text/plain')
   expect(status.success?).to be true
 end
 Given('I set {word} header to {word}') do |k, v|
